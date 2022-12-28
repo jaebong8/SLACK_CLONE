3add372533881a269ddbdd647415779f009f6def
jaebong8 <jaebong90@naver.com>
2022-12-28 18:25:24 +0900

    초기 셋팅

--- setting/ts/layouts/App.tsx
+++ setting/ts/layouts/App.tsx
@@ -1,7 +1,16 @@
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path={'/login'} component={Login} />
+      <Route path={'/signup'} component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
