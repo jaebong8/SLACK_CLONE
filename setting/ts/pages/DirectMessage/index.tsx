e87d29215a644346090c111242be22e3d38d44ee
jaebong8 <jaebong90@naver.com>
2023-01-10 17:10:40 +0900

    채팅 보내기

--- setting/ts/pages/DirectMessage/index.tsx
+++ setting/ts/pages/DirectMessage/index.tsx
@@ -3,20 +3,40 @@ import React, { useCallback } from 'react';
 import gravatar from 'gravatar';
 import { fetcher } from '@utils/fetcher';
 import useSWR from 'swr';
-import { IUser } from '@typings/db';
+import { IDM, IUser } from '@typings/db';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData, error, mutate } = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
-  const [chat, , onChangeChat] = useInput('');
+  const [chat, setChat, onChangeChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-  }, []);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            setChat('');
+          })
+          .catch((error) => {
+            console.dir(error);
+          });
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) return null;
   return (
