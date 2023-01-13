4a027541494dec4b59637fee6e897bdf6853f5d1
jaebong8 <jaebong90@naver.com>
2023-01-13 18:03:59 +0900

    dayjs 설정, custom-scrollbars 설정

--- setting/ts/components/ChatList/index.tsx
+++ setting/ts/components/ChatList/index.tsx
@@ -1,17 +1,23 @@
 import Chat from '@components/Chat';
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import { IChat, IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
